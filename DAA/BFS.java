1fc1a9da2dab31a6b9b82c66a0eaba0e5d643deb
ashithashetty2402 <ashithashetty2402@gmail.com>
2023-09-03 23:28:42 +0530

    edited

--- DAA/BFS.java
+++ DAA/BFS.java
@@ -1,53 +1,57 @@
-import java.io.*;
-import java.util.*;
-class BFS {
-private int V;
-private LinkedList<Integer> adj[];
-Graph(int v)
-{
-V = v;
-adj = new LinkedList[v];
-for (int i = 0; i < v; ++i)
-adj[i] = new LinkedList();
-}
-// Function to add an edge into the graph
-void addEdge(int v, int w) { adj[v].add(w); }
-void BFS(int s)
-{
-boolean visited[] = new boolean[V];
-// Create a queue for BFS
-LinkedList<Integer> queue
-= new LinkedList<Integer>();
-// Mark the current node as visited and enqueue it
-visited[s] = true;
-queue.add(s);
-while (queue.size() != 0) {
-// Dequeue a vertex from queue and print it
-s = queue.poll();
-System.out.print(s + " ");
-Iterator<Integer> i = adj[s].listIterator();
-while (i.hasNext()) {
-int n = i.next();
-if (!visited[n]) {
-visited[n] = true;
-queue.add(n);
-}
+import java.util.LinkedList;
+import java.util.Queue;
 
-}
-}
-}
-public static void main(String args[])
-{
-Graph g = new Graph(7);
-g.addEdge(0, 1);
-g.addEdge(0, 2);
-g.addEdge(0, 3);
-g.addEdge(1, 4);
-g.addEdge(2, 5);
-g.addEdge(3, 6);
-System.out.println("Following is Breadth First Traversal "+ "(starting from vertex 
-0)");
-g.BFS(0);}
-}
-    
+public class BFS {
+    static class Graph {
+        private int V; // Number of vertices
+        private LinkedList<Integer>[] adj; // Adjacency list
+
+        Graph(int v) {
+            V = v;
+            adj = new LinkedList[v];
+            for (int i = 0; i < v; ++i) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        void addEdge(int v, int w) {
+            adj[v].add(w);
+        }
+
+        void breadthFirstSearch(int start) {
+            boolean[] visited = new boolean[V];
+            Queue<Integer> queue = new LinkedList<>();
+
+            visited[start] = true;
+            queue.add(start);
+
+            while (!queue.isEmpty()) {
+                start = queue.poll();
+                System.out.print(start + " ");
+
+                for (Integer neighbor : adj[start]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int vertices = 7; // Change this to the number of vertices in your graph
+        Graph graph = new Graph(vertices);
+
+        // Add edges to the graph
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+
+        System.out.println("Breadth-First Traversal (starting from vertex 0):");
+        graph.breadthFirstSearch(0);
+    }
 }
